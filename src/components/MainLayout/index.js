34ca44e041f9b75a0257660a6d071ed92dc6f3ed
gtretow <guilherme.tretow@gmail.com>
2021-03-12 00:39:07 -0300

    mainlayout component

--- src/components/MainLayout/index.js
+++ src/components/MainLayout/index.js
@@ -1,4 +1,12 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+import {
+  ArrowLeft,
+  Home,
+  Search,
+  Notifications,
+  Email,
+} from "../../styles/icons";
 
 export const Container = styled.div`
   /* always mobile first */
@@ -9,12 +17,76 @@ export const Container = styled.div`
     width: 50%;
   } ;
 `;
-export const Header = styled.div``;
-export const Bio = styled.div``;
-export const RSideBar = styled.div``;
+export const Header = styled.div`
+  > button {
+    padding: 8px;
+    border-radius: 50%;
+    outline: 0;
+  }
+  /* Sticky positioning is a hybrid of relative and fixed positioning. The element is treated as relative positioned until it crosses a specified threshold, at which point it is treated as fixed positioned. */
+  position: sticky;
+  display: flex;
+  align-items: center;
+  text-align: left;
+  padding: 9px 0 9px 13 px;
+  border-bottom: 1px solid var(--outline);
+  z-index: 2;
+  top: 0;
+`;
+export const Bio = styled.div`
+  margin-left: 17px;
+  display: flex;
+  flex-direction: column;
+  > strong {
+    font-size: 1.3rem;
+  }
 
-export const HomeIcon = styled.div``;
-export const SearchIcon = styled.div``;
-export const BellIcon = styled.div``;
-export const EmailIcon = styled.div``;
-export const BackIcon = styled.div``;
+  > span {
+    font-size: 1rem;
+    color: var(--gray);
+  }
+`;
+
+export const RSideBar = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  z-index: 2;
+
+  @media (min-width: 500px) {
+    display: none;
+  }
+  width: 100%;
+  border-top: 1px solid var(--outline);
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 30px;
+`;
+
+const iconCSS = css`
+  width: 31px;
+  height: 31px;
+
+  cursor: pointer;
+
+  fill: var(--gray);
+  &:hover &.active {
+    fill: var(--twitter);
+  }
+`;
+
+export const HomeIcon = styled(Home)`
+  ${iconCSS}
+`;
+export const SearchIcon = styled(Search)`
+  ${iconCSS}
+`;
+export const BellIcon = styled(Notifications)`
+  ${iconCSS}
+`;
+export const EmailIcon = styled(Email)`
+  ${iconCSS}
+`;
+export const BackIcon = styled(ArrowLeft)`
+  ${iconCSS}
+`;
